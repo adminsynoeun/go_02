519852cf46d4a1b466de3a3c2d87d9231a1140ca
adminsynoeun <151695341+adminsynoeun@users.noreply.github.com>
2024-08-19 11:00:17 +0700

    Initial commit

--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-
-	"github.com/adminsynoeun/go_02/blob/main/utils/math.go"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/person", Add).Methods("GET")
+	r.HandleFunc("/person", debugJSON).Methods("GET")
 
-	// Start the HTTP server on port 8090
 	log.Println("Server listening on :8090")
 	log.Fatal(http.ListenAndServe(":8090", r))
 }
+type Person struct {
+	Name string
+	Age  int
+  }
+func debugJSON(w http.ResponseWriter, r *http.Request) {
+	person := Person{"John Doe", 30}
+	json, err := json.Marshal(person)
+	if err != nil {
+	  http.Error(w, err.Error(), http.StatusInternalServerError)
+	  return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+  }
